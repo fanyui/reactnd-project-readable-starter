8c51eaf39dce1cfd38714157a89041c23f8a71a5
Harisu Fanyui <fanyuiharisu@gmail.com>
2020-05-07 16:51:34 +0100

    Added differntial fetch of post based on category

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ function App() {
           <LoadingBar />
         <div className='container'>
           <Route path='/' exact component={Home} />
+          <Route path='/:category' exact component={Home} />
           <Route path='/post/:id' exact component={Post}/>
 
         </div>
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect} from 'react'
 import Posts from './component/Posts'
+import { useParams } from 'react-router-dom'
+import { useDispatch, } from 'react-redux'
+import { handleReceivePosts } from './actions/post'
 function Home(props) {
+    const dispatch = useDispatch()
+    let {category} = useParams()
+    useEffect(() => {
+        if(!category){
+            dispatch(handleReceivePosts("all"))
+
+        }
+        else{
+            dispatch(handleReceivePosts(category))
+
+        }
+    }, [category])
     return (
         <div>
             <Posts />
